1d64389444fd983a7294b91fc66807f97dfdfbc6
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-22 15:36:47 +0300

    Fix build timeout for homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import React from 'react';
 import MainGridContentDesktop from '@/components/MainGridContentDesktop';
 import PageContainer from '@/components/PageContainer';
