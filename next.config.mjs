a61905ed2ad5381beb2b7e1cfc204d80464502f5
Yosef Sabag <yosefsabag46@gmail.com>
2025-10-30 21:05:11 +0200

    load

--- next.config.mjs
+++ next.config.mjs
@@ -1,96 +1,32 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      // 🔹 Strapi (לוקאל)
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-
-      // 🔹 Shopify
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-      },
-
-      // 🔹 YouTube (רגיל + Shorts)
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.youtube.com",
-      },
-
-      // 🔹 TikTok
-      {
-        protocol: "https",
-        hostname: "*.tiktokcdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.tiktokcdn-us.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.tiktokcdn-va.com",
-      },
-
-      // 🔹 Instagram / Facebook
-      {
-        protocol: "https",
-        hostname: "*.cdninstagram.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.fbcdn.net",
-      },
-
-      // 🔹 Twitter (X)
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.twimg.com",
-      },
-
-      // 🔹 Googleusercontent
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-
-      // 🔹 Fullgaz
-      {
-        protocol: "https",
-        hostname: "fullgaz.co.il",
-      },
-      {
-        protocol: "https",
-        hostname: "www.fullgaz.co.il",
-      },
-
-      // 🔹 כללית – לכל דומיין https
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      // 🔹 כללית – לכל דומיין http
-      {
-        protocol: "http",
-        hostname: "**",
-      },
+    unoptimized: true, // ✅ ביטול מוחלט של Next.js Image Optimization
+    domains: [
+      "localhost",
+      "cdn.shopify.com",
+      "i.ytimg.com",
+      "img.youtube.com",
+      "*.tiktokcdn.com",
+      "*.tiktokcdn-us.com",
+      "*.tiktokcdn-va.com",
+      "*.cdninstagram.com",
+      "*.fbcdn.net",
+      "pbs.twimg.com",
+      "*.twimg.com",
+      "*.googleusercontent.com",
+      "lh3.googleusercontent.com",
+      "fullgaz.co.il",
+      "www.fullgaz.co.il",
+      "press.ktm.com",
+      "res.cloudinary.com",
+      "onmotormedia.com",
+      "husqvarna-motorcycles.com",
+      "gasgas.com",
+      "ducati.com",
+      "yamaha-motor.eu",
+      "honda.com",
+      "bmw-motorrad.com",
     ],
   },
 };
