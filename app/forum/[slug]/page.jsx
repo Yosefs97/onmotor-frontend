f2fbc5b488508fd3390e758fdc2c6f206427bc85
Yosef Sabag <yosefsabag46@gmail.com>
2025-11-11 11:04:57 +0200

    load

--- app/forum/[slug]/page.jsx
+++ app/forum/[slug]/page.jsx
@@ -73,7 +73,7 @@ export default function ForumCategoryPage() {
               categorySlug={slug}
               onCreated={() => {
                 setShowForm(false);
-                location.reload();
+                loadThreads();
               }}
             />
           </div>
