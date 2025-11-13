64d1b1efe018274413c3b92ff6766c5a603cbd8f
Yosef Sabag <yosefsabag46@gmail.com>
2025-11-13 23:12:23 +0200

    lod

--- app/page.js
+++ app/page.js
@@ -54,11 +54,11 @@ export default async function HomePage() {
       <MainGridContentDesktop articles={articles} />
 
       <h1 className="text-2xl font-bold text-[#e60000] px-4 mt-4">
-        OnMotor Media – מגזין אופנועים ישראלי
+        OnMotor Media - מגזין אופנועים ישראלי
       </h1>
 
       <p className="px-4 mt-2 mb-4 text-gray-700">
-         מגזין אופנועים בישראל – חדשות, סקירות, מבחני דרכים, ציוד, טיפים לקהילת הרוכבים התוססת. בישראל
+         .מגזין אופנועים בישראל - חדשות, סקירות, מבחני דרכים, ציוד, טיפים לקהילת הרוכבים התוססת בישראל
       </p>
 
     </PageContainer>
