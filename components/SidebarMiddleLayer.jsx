b4392b879fb07f346548a2650f6c829971306b32
Yosef Sabag <yosefsabag46@gmail.com>
2025-11-23 13:42:49 +0200

    load

--- components/SidebarMiddleLayer.jsx
+++ components/SidebarMiddleLayer.jsx
@@ -1,40 +1,17 @@
+// components/SidebarMiddleLayer.jsx
+'use client';
+
 import SidebarFixed from './SidebarFixed';
 import ContactForAdBox from './ContactForAdBox';
 
-const STRAPI_URL =
-  process.env.STRAPI_API_URL ||
-  process.env.NEXT_PUBLIC_STRAPI_API_URL;
-
-async function getSidebarAds() {
-  if (!STRAPI_URL) return [];
-
-  const res = await fetch(
-    `${STRAPI_URL}/api/sidebar-middles?populate=image&populate=video`,
-    { next: { revalidate: 120 } }
-  );
-
-  if (!res.ok) return [];
-
-  const json = await res.json();
-
-  return (
-    json.data?.map((item) => ({
-      id: item.id,
-      ...item.attributes,
-    })) || []
-  );
-}
-
-export default async function SidebarMiddleLayer() {
-  const ads = await getSidebarAds();
-
+export default function SidebarMiddleLayer() {
   return (
     <div className="sidebar-middle-layer sticky top-7 flex flex-col gap-0 w-full flex-1">
       <div className="flex-1">
-        <SidebarFixed ads={ads} />
+        <SidebarFixed />
       </div>
 
-      <div className=" bottom-0 bg-gray-900">
+      <div className="bottom-0 bg-gray-900">
         <ContactForAdBox />
       </div>
     </div>
