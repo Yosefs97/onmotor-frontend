a9ab8cea765f5097ea5c22629203b6ef47ce4d50
Yosef Sabag <yosefsabag46@gmail.com>
2025-12-16 17:44:31 +0200

    load

--- app/shop/cart/page.jsx
+++ app/shop/cart/page.jsx
@@ -1,6 +1,7 @@
 // /app/shop/cart/page.jsx
-export const dynamic = 'force-dynamic';
 'use client';
+export const dynamic = 'force-dynamic';
+
 import Link from "next/link";
 import { useEffect, useState } from 'react';
 import ShopSidebar from '@/components/ShopSidebar';
