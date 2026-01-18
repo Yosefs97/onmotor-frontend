9809d6a8783f1fdd96fdd0a3a1f6ec3e0e561db7
Yosef Sabag <yosefsabag46@gmail.com>
2026-01-18 16:31:27 +0200

    load

--- components/CommentsSection.jsx
+++ components/CommentsSection.jsx
@@ -5,8 +5,8 @@ import FacebookComments from './FacebookComments';
 
 export default function CommentsSection({ articleUrl }) {
   return (
-    <div className="w-full mt-12" dir="rtl">
-      <h3 className="text-xl font-bold border-red-600 border-t-2 pb-2">
+    <div className="w-full mt-2" dir="rtl">
+      <h3 className="text-xl font-bold border-red-600 border-t-2 pb-0.2">
         תגובות הגולשים
       </h3>
 
