0c6e2ebbdd18e3d550699ac33c021c7c7227bb72
Yosef Sabag <yosefsabag46@gmail.com>
2026-01-19 12:23:12 +0200

    load

--- app/layout.js
+++ app/layout.js
@@ -34,6 +34,7 @@ export const metadata = {
     icon: [
       { url: '/favicon.ico', sizes: 'any' }, 
       { url: '/favicon_32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
