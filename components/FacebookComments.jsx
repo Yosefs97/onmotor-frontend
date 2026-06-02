a184c1ed67e4c2a7f6b107135288b985b6b34e70
Yosef Sabag <yosefsabag46@gmail.com>
2026-06-02 13:54:44 +0300

    load

--- components/FacebookComments.jsx
+++ components/FacebookComments.jsx
@@ -6,22 +6,19 @@ import Script from 'next/script';
 export default function FacebookComments({ url }) {
   const [pageUrl, setPageUrl] = useState('');
 
-  // 1. הגדרת הכתובת שעליה מגיבים
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // עדיף להשתמש קודם כל ב-url שהועבר מ-CommentsSection
       setPageUrl(url || window.location.href.split('?')[0]);
     }
   }, [url]);
 
-  // 2. פונקציה שתופעל בדיוק כשהסקריפט של פייסבוק יסיים להיטען
-  const handleFacebookLoad = () => {
+  const initFacebook = () => {
+    // וידוא שהאובייקט של פייסבוק קיים לפני שמנסים לרנדר
     if (window.FB) {
       window.FB.XFBML.parse();
     }
   };
 
-  // 3. תמיכה במעבר בין עמודים (SPA)
   useEffect(() => {
     if (pageUrl && window.FB) {
       window.FB.XFBML.parse();
@@ -29,26 +26,25 @@ export default function FacebookComments({ url }) {
   }, [pageUrl]);
 
   return (
-    <div className="mt-2">
-      {/* טעינת הסקריפט של פייסבוק.
-        השפה כאן היא he_IL (עברית). 
-        אסטרטגיית lazyOnload מבטיחה שהאתר שלך לא יואט בגלל פייסבוק.
-      */}
+    <div className="mt-2 w-full">
+      {/* 1. חובה: אלמנט שפייסבוק דורש כדי לפעול */}
+      <div id="fb-root"></div>
+
+      {/* 2. שינינו ל-afterInteractive */}
       <Script
         src="https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v19.0"
-        strategy="lazyOnload"
-        onLoad={handleFacebookLoad}
+        strategy="afterInteractive"
+        onLoad={initFacebook}
         crossOrigin="anonymous"
       />
 
-      {/* הקונטיינר של פייסבוק - חשוב שהוא יהיה קיים ב-DOM כשהפונקציה parse רצה */}
+      {/* 3. הקונטיינר של התגובות */}
       {pageUrl && (
         <div
-          className="fb-comments"
+          className="fb-comments w-full"
           data-href={pageUrl}
           data-width="100%"
           data-numposts="5"
-          data-order-by="reverse_time"
           data-mobile="true"
         />
       )}
