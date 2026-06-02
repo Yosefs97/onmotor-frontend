5d09e6bbe5383a87e97c6a18e40610c9e4a80702
Yosef Sabag <yosefsabag46@gmail.com>
2026-06-02 14:22:50 +0300

    load

--- components/FacebookComments.jsx
+++ components/FacebookComments.jsx
@@ -1,53 +1,51 @@
 // components/FacebookComments.jsx
 'use client';
-import { useEffect, useState } from 'react';
-import Script from 'next/script';
+import { useEffect, useState, useRef } from 'react';
 
 export default function FacebookComments({ url }) {
   const [pageUrl, setPageUrl] = useState('');
+  const containerRef = useRef(null);
 
+  // 1. קביעת הכתובת שעליה מגיבים
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setPageUrl(url || window.location.href.split('?')[0]);
     }
   }, [url]);
 
-  const initFacebook = () => {
-    // וידוא שהאובייקט של פייסבוק קיים לפני שמנסים לרנדר
-    if (window.FB) {
-      window.FB.XFBML.parse();
-    }
-  };
-
+  // 2. קריאה לפייסבוק לרנדר את התגובות
   useEffect(() => {
-    if (pageUrl && window.FB) {
-      window.FB.XFBML.parse();
-    }
+    // אנחנו נותנים ל-React רגע לסיים לצייר את ה-DOM עם ה-setTimeout
+    const timeoutId = setTimeout(() => {
+      if (pageUrl && window.FB && window.FB.XFBML) {
+        // אם תפסנו את הקונטיינר, נבקש מפייסבוק לסרוק רק אותו
+        if (containerRef.current) {
+          window.FB.XFBML.parse(containerRef.current);
+        } else {
+          window.FB.XFBML.parse();
+        }
+      }
+    }, 200);
+
+    return () => clearTimeout(timeoutId);
   }, [pageUrl]);
 
+  if (!pageUrl) return null;
+
   return (
-    <div className="mt-2 w-full">
-      {/* 1. חובה: אלמנט שפייסבוק דורש כדי לפעול */}
-      <div id="fb-root"></div>
-
-      {/* 2. שינינו ל-afterInteractive */}
-      <Script
-        src="https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v19.0"
-        strategy="afterInteractive"
-        onLoad={initFacebook}
-        crossOrigin="anonymous"
+    <div className="mt-4 w-full min-h-[150px]" ref={containerRef}>
+      {/* השימוש ב-key עם הכתובת הוא קריטי! 
+        הוא אומר ל-React: "אם הכתובת השתנתה, תמחק את הדיב הישן ותייצר אחד חדש לגמרי".
+        זה מונע מפייסבוק להיתקע על גובה 0.
+      */}
+      <div
+        key={pageUrl}
+        className="fb-comments w-full"
+        data-href={pageUrl}
+        data-width="100%"
+        data-numposts="5"
+        data-mobile="true"
       />
-
-      {/* 3. הקונטיינר של התגובות */}
-      {pageUrl && (
-        <div
-          className="fb-comments w-full"
-          data-href={pageUrl}
-          data-width=""
-          data-numposts="5"
-          data-mobile="true"
-        />
-      )}
     </div>
   );
 }
